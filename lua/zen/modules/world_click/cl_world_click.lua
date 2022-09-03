a4878044c9acb531f1b10d1b842e2e2d140025a3
jtaylerj <jtaylerj@gmail.com>
2022-09-03 14:16:17 +0100

    update world_click

--- lua/zen/modules/world_click/cl_world_click.lua
+++ lua/zen/modules/world_click/cl_world_click.lua
@@ -13,66 +13,45 @@ function worldclick.CheckHover()
     end
 end
 
-function worldclick.CheckClick(code)
-    if not LocalPlayer():izen_HasPerm("zen.worldclick") then return end
+function worldclick.CheckClick(ply, code)
+    if code < MOUSE_FIRST or code > MOUSE_LAST then return end
+    if not ply:izen_HasPerm("zen.worldclick") then return end
+    if not vgui.CursorVisible() then return end
 
     local hover_pnl = vgui.GetHoveredPanel()
-    if not IsValid(hover_pnl) or not hover_pnl:IsWorldClicker() then return end
-
-    hook.Run("zen.worldclick.onPress", code, worldclick.tLastTrace)
-    if IsValid(worldclick.objLastEntity) then
-        hook.Run("zen.worldclick.onPressEntity", worldclick.objLastEntity, code, worldclick.tLastTrace)
+    if IsValid(hover_pnl) and hover_pnl != vgui.GetWorldPanel() and hover_pnl != g_ContextMenu then
+        if hover_pnl:IsWorldClicker() then
+            hook.Run("zen.worldclick.onPress", code, worldclick.tLastTrace)
+            if IsValid(worldclick.objLastEntity) then
+                hook.Run("zen.worldclick.onPressEntity", worldclick.objLastEntity, code, worldclick.tLastTrace)
+            end
+        else
+            hook.Run("zen.worldclick.panel.onPress", code)
+        end
+    else
+        hook.Run("zen.worldclick.nopanel.onPress", code)
     end
 end
-hook.Add( "GUIMousePressed", "zen.worldclick", worldclick.CheckClick)
+hook.Add( "PlayerButtonPress", "zen.worldclick", worldclick.CheckClick)
 
-function worldclick.CheckUnClick(code)
-    if not LocalPlayer():izen_HasPerm("zen.worldclick") then return end
+function worldclick.CheckUnClick(ply, code)
+    if code < MOUSE_FIRST or code > MOUSE_LAST then return end
+    if not ply:izen_HasPerm("zen.worldclick") then return end
+    if not vgui.CursorVisible() then return end
+
 
     local hover_pnl = vgui.GetHoveredPanel()
-    if not IsValid(hover_pnl) or not hover_pnl:IsWorldClicker() then return end
-
-    hook.Run("zen.worldclick.onRelease", code, worldclick.tLastTrace)
-    if IsValid(worldclick.objLastEntity) then
-        hook.Run("zen.worldclick.onReleaseEntity", worldclick.objLastEntity, code, worldclick.tLastTrace)
+    if IsValid(hover_pnl) and hover_pnl != vgui.GetWorldPanel() and hover_pnl != g_ContextMenu then
+        if hover_pnl:IsWorldClicker() then
+            hook.Run("zen.worldclick.onRelease", code, worldclick.tLastTrace)
+            if IsValid(worldclick.objLastEntity) then
+                hook.Run("zen.worldclick.onReleaseEntity", worldclick.objLastEntity, code, worldclick.tLastTrace)
+            end
+        else
+            hook.Run("zen.worldclick.panel.onRelease", code)
+        end
+    else
+        hook.Run("zen.worldclick.nopanel.onRelease", code)
     end
 end
-hook.Add( "GUIMouseReleased", "zen.worldclick", worldclick.CheckUnClick)
-
-hook.Add( "PreventScreenClicks", "zen.worldclick", function()
-    local hover_pnl = vgui.GetHoveredPanel()
-    if not IsValid(hover_pnl) or not hover_pnl:IsWorldClicker() then return end
-
-    return LocalPlayer():izen_HasPerm("zen.worldclick")
-end)
-
-local color_hover = Color(255, 0, 0)
-hook.Add( "PreDrawEffects", "zen.variable_edit", function()
-    local hover_pnl = vgui.GetHoveredPanel()
-    if not IsValid(hover_pnl) or not hover_pnl:IsWorldClicker() then return end
-
-    worldclick.CheckHover()
-
-    local ent = worldclick.objLastEntity
-    if ( !IsValid( ent ) ) then return end
-
-    local origin = util.GetPlayerTraceSource(nil, true)
-
-    local pos = ent:WorldSpaceCenter()
-    local ang = ((pos) - origin):Angle()
-    ang.p = 0
-    ang.r = 90
-    ang.y = ang.y - 90
-
-    cam.Start3D2D(pos, ang, 0.5)
-        cam.IgnoreZ(true)
-        draw.SimpleText(ent:GetClass(), "DebugOverlay", 0, 0, color_white, 1,1)
-        cam.IgnoreZ(false)
-    cam.End3D2D()
-
-
-    hook.Run("zen.worldclick.DrawEntityInfo", ent, pos, ang)
-end)
-
-hook.Add("zen.worldclick.onPress", "zen.nt", function(code, tr) nt.Send("zen.worldclick.onPress", {"uint7", "vector", "vector"}, {code, tr.StartPos, tr.Normal}) end)
-hook.Add("zen.worldclick.onRelease", "zen.nt", function(code, tr) nt.Send("zen.worldclick.onRelease", {"uint7", "vector", "vector"}, {code, tr.StartPos, tr.Normal}) end)
+hook.Add( "PlayerButtonUnPress", "zen.worldclick", worldclick.CheckUnClick)

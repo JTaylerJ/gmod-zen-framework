2da069dedd51983535cc22dcb824e23a460b3c50
jtaylerj <jtaylerj@gmail.com>
2022-10-08 10:10:22 +0300

    add html, collapse_down panels

--- lua/zen/lib/interface/gui/cl_panels.lua
+++ lua/zen/lib/interface/gui/cl_panels.lua
@@ -15,6 +15,8 @@ gui.RegisterStylePanel("list", {}, "DScrollPanel", {"dock_fill"}, {})
 gui.RegisterStylePanel("button", {}, "DButton", {text = "zen.button"}, {})
 gui.RegisterStylePanel("entry", {}, "DTextEntry", {font = ui.ffont(8)}, {})
 
+gui.RegisterStylePanel("html", {}, "DHTML", {}, {})
+
 gui.RegisterStylePanel("func_panel", {}, "EditablePanel", {mouse_input = false})
 
 
@@ -23,3 +25,37 @@ gui.RegisterStylePanel("white_fill", {
         draw.Box(0,0,w,h,COLOR.WHITE)
     end
 }, "EditablePanel", {"dock_fill"}, {})
+
+
+gui.RegisterStylePanel("collapse_down", {
+    zen_PreInit = function(self)
+        self.pnl_Collapse = self:zen_AddStyled("base", {
+            "dock_top", tall = 10,
+            cc = {
+                bCollapse = true,
+                Paint = function(self, w, h)
+                    if self:IsHovered() then draw.Box(0,0,w,h,COLOR.G) end
+                    draw.Text("Collapse", 6, w/2, h/2, COLOR.W, 1, 1, COLOR.BLACK)
+                end,
+                OnMousePressed = function(self)
+                    owner = self:GetParent()
+                    self.bCollapse = !self.bCollapse
+                    if self.bCollapse then
+                        owner:SizeTo(-1, 10, 0.5)
+                    else
+                        local _, h = owner:ChildrenSize()
+                        owner:SizeTo(-1, owner.lH, 0.5)
+                    end
+                end,
+            }
+        })
+    end,
+    PerformLayout = function(self, w, h)
+        self.lW = w
+        self.lH = math.max(h, self.lH or 0)
+    end,
+    zen_PostInit = function(self)
+        self.lH = math.max(self:GetTall(), self.lH or 0)
+        self:SetTall(10)
+    end
+}, "EditablePanel", {font = ui.ffont(8)}, {})

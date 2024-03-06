5667b486cc59a9208f181bd9c1bfbe7b7b8c2023
(Master-Config 001) <jtaylerj@gmail.com>
2024-03-07 02:44:04 +0300

    Update move tool

--- lua/zen/modules/map_edit/tool_mode/tools/move/cl_move.lua
+++ lua/zen/modules/map_edit/tool_mode/tools/move/cl_move.lua
@@ -10,11 +10,16 @@ TOOL.Description = "Move Entity"
 function TOOL:Init()
 end
 
-function TOOL:Render(rendermode, priority, vw)
+function TOOL:Think(rendermode, priority, vw)
     local bGrabbingActive = IsValid(self.eGrabbedEntity)
     if bGrabbingActive then
+        self.vNewPosition = map_edit.GetHoverOrigin()
 
-
+        self:CallServerAction{
+            action = "update_pos",
+            pos = self.vNewPosition,
+            ang = self.vNewAngle
+        }
     end
 end
 
@@ -25,31 +30,59 @@ function TOOL:GrabEntity()
     local ent = map_edit.GetHoverEntity()
     if !IsValid(ent) then return end
 
+    self.eGrabbedEntity = ent
+    print("Grabbed Entity: ", ent)
+
     self:CallServerAction{
         action = "grab",
         ent = ent
     }
 
-    self.eGrabbedEntity = ent
+    self.vGrabOffset = self.eGrabbedEntity:GetPos() - map_edit.GetHoverOrigin()
+    self.iLastMoveType = self.iLastMoveType or ent:GetMoveType()
+    self.iLastSolid = self.iLastSolid or ent:GetSolid()
+    ent:SetMoveType(MOVETYPE_CUSTOM)
+    ent:SetSolid(SOLID_NONE)
 
     self.vStartPos = self.eGrabbedEntity:GetPos()
     self.aStartAngle = self.eGrabbedEntity:GetAngles()
+
+    self.vNewAngle = self.aStartAngle
+    self.vNewPosition = map_edit.GetHoverOrigin()
 end
 
 function TOOL:UnGrabEntity()
     if !self.eGrabbedEntity then return end
     if !IsValid(self.eGrabbedEntity) then return end
 
+    local ent = self.eGrabbedEntity
+    local movetype = self.iLastMoveType
+    local solid = self.iLastSolid
+
+    self.vGrabOffset = nil
+    self.eGrabbedEntity = nil
+    self.iLastMoveType = nil
+    self.iLastSolid = nil
+    print("UnGrabbed Entity: ", ent)
+
     self:CallServerAction{
         action = "ungrab",
+        solid = solid,
+        movetype = movetype
     }
+end
 
-    self.eGrabbedEntity = nil
+function TOOL:ResetAngle()
+    self.vNewAngle = Angle(0,0,0)
 end
 
 function TOOL:OnButtonPress(but, in_key, bind_name, vw)
     if bind_name == "+attack" then
         self:GrabEntity()
+    end
+
+    if bind_name == "+walk" then
+        self.bAngleRotaing = true
     end
 end
 
@@ -57,6 +90,15 @@ function TOOL:OnButtonUnPress(but, in_key, bind_name, vw)
     if bind_name == "+attack" then
         self:UnGrabEntity()
     end
+
+    if bind_name == "+reload" then
+        self:ResetAngle()
+    end
+
+    if bind_name == "+walk" then
+        self.bAngleRotaing = false
+    end
+
 end
 
 map_edit.tool_mode.Register(TOOL)
--- lua/zen/modules/map_edit/tool_mode/tools/move/sv_move.lua
+++ lua/zen/modules/map_edit/tool_mode/tools/move/sv_move.lua
@@ -11,16 +11,37 @@ function TOOL:Init()
 end
 
 function TOOL:UpdateViewPos(data)
-    
+    local pos = data.pos
+    local ang = data.ang
+    local ent = self.eGrabbedEntity
+
+    if IsValid(ent) then
+        ent:SetPos(pos)
+        ent:SetAngles(ang)
+    end
+
 end
 
 function TOOL:Grab(data)
     local ent = data.ent
+
     if !IsValid(ent) then return end
+
+    self.eGrabbedEntity = ent
+
+    ent:SetMoveType(MOVETYPE_CUSTOM)
+    ent:SetSolid(SOLID_NONE)
 end
 
 function TOOL:Ungrab(data)
-    
+    local ent = self.eGrabbedEntity
+
+    self.eGrabbedEntity = nil
+
+    if IsValid(ent) then
+        ent:PhysicsInit(data.solid)
+        ent:SetMoveType(data.movetype)
+    end
 end
 
 
@@ -28,7 +49,7 @@ function TOOL:ServerAction(data)
     local action = data.action
     if !action then return end
 
-    if action == "update_viewpos"then
+    if action == "update_pos"then
         self:UpdateViewPos(data)
     end
 

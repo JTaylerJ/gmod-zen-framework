317f3b1249e481b9e11b8f3b04a26abbe5010bcb
jtaylerj <jtaylerj@gmail.com>
2024-04-17 00:06:41 -0700

    Setup default config

--- lua/zen/config.lua
+++ lua/zen/config.lua
@@ -1,9 +1,11 @@
 module("zen", package.seeall)
 
--- List of admins: SteamID64 [String] = Value [boolean]
-_CFG.Admins = {
-    ["76561198272243731"] = true -- Addon creator: -243 King
-}
+if !_CFG.bFirstInitialized then
+    _CFG.bFirstInitialized = nil
 
--- Request autorization before use admin access
-_CFG.Admin_AuthorizationRequire = false
+    -- List of admins: SteamID64 [String] = Value [boolean]
+    _CFG.Admins = {}
+
+    -- Request autorization before use admin access
+    _CFG.Admin_AuthorizationRequire = true
+end

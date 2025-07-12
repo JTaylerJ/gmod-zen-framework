f0b1eb63cae957be63de1c5c237a473191f9a872
JTaylerJ <jtaylerj@gmail.com>
2025-07-12 03:25:52 -0700

    Update zen loader

--- lua/autorun/sh_zen_loader.lua
+++ lua/autorun/sh_zen_loader.lua
@@ -1,10 +1,2 @@
-if SERVER then
-    AddCSLuaFile("zen/main/_main.lua")
-    include("zen/main/_main.lua")
-end
-
-if CLIENT_DLL then
-    concommand.Add("+zen_tools", function()
-        include("zen/main/_main.lua")
-    end)
-end
+AddCSLuaFile("zen/__main__.lua")
+include("zen/__main__.lua")

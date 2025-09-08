99082aef9dc4e1033ec000c9770806949bc0de80
JTaylerJ <jtaylerj@gmail.com>
2025-09-08 04:27:17 -0700

    Add Generation Settings and MenuActive check

--- lua/zen_plugin/map_edit/lib/cl_icon_generation.lua
+++ lua/zen_plugin/map_edit/lib/cl_icon_generation.lua
@@ -9,7 +9,7 @@ local wireframe_mat = Material(weapon_material)
 -- Generate PNG Data for a given entity class
 ---@overload fun(weapon_class: string, callback: fun(success: true, png_data: string))
 ---@overload fun(weapon_class: string, callback: fun(success: false, err: string))
-function icon_generation.generateWeapon(weapon_class, callback)
+function icon_generation.generateWeapon(weapon_class, callback, GenerationSettings)
     callback = callback or function(succ, data)
         if !succ then
             print("icon_generation.generateWeapon: Failed to generate icon for weapon class " .. tostring(weapon_class) .. ": " .. tostring(data))
@@ -28,103 +28,150 @@ function icon_generation.generateWeapon(weapon_class, callback)
         return
     end
 
-    local CSEnt = ents.CreateClientProp(WorldModel)
-    if !IsValid(CSEnt) then
-        callback(false, "Failed to create clientside entity for model " .. tostring(WorldModel))
-        return
-    end
+    local render_id = "icon_generation_rt_" .. weapon_class
+    hook.Add("PostRender", render_id, function()
+        if gui.IsGameUIVisible() then return end
 
-    icon_generation.LastCreatedEntity = CSEnt
+        hook.Remove("PostRender", render_id)
 
-    SafeRemoveEntityDelayed(CSEnt, 1)
+        local CSEnt = ents.CreateClientProp(WorldModel)
+        if !IsValid(CSEnt) then
+            callback(false, "Failed to create clientside entity for model " .. tostring(WorldModel))
+            return
+        end
 
-    local phys = CSEnt:GetPhysicsObject()
-    if IsValid(phys) then
-        phys:EnableMotion(false)
-    end
+        icon_generation.LastCreatedEntity = CSEnt
 
-    CSEnt:SetMaterial(weapon_material)
-    CSEnt:DrawShadow(false)
-    CSEnt:SetupBones()
-    CSEnt:SetNoDraw(true)
+        SafeRemoveEntityDelayed(CSEnt, 1)
 
-    local _mins, _maxs = CSEnt:GetRenderBounds()
-    local _middle = (_mins + _maxs) / 2
+        local phys = CSEnt:GetPhysicsObject()
+        if IsValid(phys) then
+            phys:EnableMotion(false)
+        end
 
-    local wep_origin = _middle
-    local wep_angles = Angle(0,0,0)
+        GenerationSettings = GenerationSettings or {}
 
+        GenerationSettings.fov = GenerationSettings.fov or 70
 
-    local size = 0
-    for i = 1, 3 do
-        size = math.max(size, math.abs(_mins[i]) + math.abs(_maxs[i]) + math.abs(_middle[i]))
-    end
-
-    size = math.max(size, 50)
+        if GenerationSettings.ModelMaterial then CSEnt:SetMaterial(GenerationSettings.ModelMaterial) end
+        CSEnt:DrawShadow(false)
+        CSEnt:SetupBones()
+        CSEnt:SetNoDraw(true)
 
 
-    local cam_pos = wep_origin + Vector(0, size/1.5, 0)
-    local cam_ang = (wep_origin - cam_pos):Angle()
+        local _mins, _maxs = CSEnt:GetRenderBounds()
+        local _middle = (_mins + _maxs) / 2
+
+        local wep_origin = _middle
+        local wep_angles = Angle(0,0,0)
 
 
-    local CAM = {}
-    CAM.type = "3D"
-    CAM.fov = 70
-    CAM.znear = 1
-    CAM.zfar = 1000
-    CAM.origin = cam_pos
-    CAM.angles = cam_ang
-    CAM.subrect = true
-
-    CAM.x = 0
-    CAM.y = 0
-    CAM.w = 500
-    CAM.h = 300
-
-    CAM.aspect = CAM.w / CAM.h
+        if GenerationSettings.OffsetRight then
+            wep_origin = wep_origin + wep_angles:Right() * GenerationSettings.OffsetRight
+        end
+        if GenerationSettings.OffsetUp then
+            wep_origin = wep_origin + wep_angles:Up() * GenerationSettings.OffsetUp
+        end
+        if GenerationSettings.OffsetForward then
+            wep_origin = wep_origin + wep_angles:Forward() * GenerationSettings.OffsetForward
+        end
 
 
-    cam.Start(CAM)
-        render.Clear(0,0,0, 0, true, true)
+        local size = 0
+        for i = 1, 3 do
+            size = math.max(size, math.abs(_mins[i]) + math.abs(_maxs[i]) + math.abs(_middle[i]))
+        end
 
-        render.SuppressEngineLighting( true )
-        render.OverrideAlphaWriteEnable(true, true)
-        render.SetWriteDepthToDestAlpha( false )
-
-        render.ModelMaterialOverride(wireframe_mat)
-
-        CSEnt:SetMaterial(weapon_material)
-        CSEnt:DrawModel()
-        CSEnt:FrameAdvance()
+        size = math.max(size, 50)
 
 
-        local PNG = render.Capture({
-            format = "png",
-            quality = 100,
-            x = 0,
-            y = 0,
-            w = CAM.w,
-            h = CAM.h,
-        })
-
-        render.SuppressEngineLighting( false )
-        render.OverrideAlphaWriteEnable(false, false)
-        render.ModelMaterialOverride(nil)
-        render.SetWriteDepthToDestAlpha(true)
+        local cam_pos = wep_origin + Vector(0, size/1.5, 0)
+        local cam_ang = (wep_origin - cam_pos):Angle()
 
 
-        callback(true, PNG)
-    cam.End()
+        local CAM = {}
+        CAM.type = "3D"
+        CAM.fov = GenerationSettings.fov
+        CAM.znear = 1
+        CAM.zfar = 1000
+        CAM.origin = cam_pos
+        CAM.angles = cam_ang
+        CAM.subrect = false
+
+        CAM.x = 0
+        CAM.y = 0
+        CAM.w = 500
+        CAM.h = 300
+
+        CAM.aspect = CAM.w / CAM.h
+
+
+        local texture = GetRenderTargetEx("icon_generation_rt_",
+            CAM.w, CAM.h,
+            RT_SIZE_NO_CHANGE, -- Just no touch anything
+            MATERIAL_RT_DEPTH_SHARED, -- Alpha use multiply alpha object. If any bags then change to --> MATERIAL_RT_DEPTH_SEPARATE --> MATERIAL_RT_DEPTH_ONLY
+            1 + 256, -- Best Combo to enable high-equility screenshot
+            0, -- Dont tested
+            IMAGE_FORMAT_RGBA16161616 -- Allow use more colors in game. Default game colors is restricted!
+        )
+
+        render.PushRenderTarget(texture)
+            cam.Start(CAM)
+                render.Clear(0,0,0, 0, true, true)
+
+                render.SuppressEngineLighting( true )
+                render.OverrideAlphaWriteEnable(true, true)
+                render.SetWriteDepthToDestAlpha( false )
+
+                render.ModelMaterialOverride(wireframe_mat)
+
+                CSEnt:SetMaterial(weapon_material)
+                CSEnt:DrawModel()
+                CSEnt:FrameAdvance()
+
+
+                local PNG = render.Capture({
+                    format = "png",
+                    quality = 100,
+                    x = 0,
+                    y = 0,
+                    w = CAM.w,
+                    h = CAM.h,
+                })
+
+
+
+                render.SuppressEngineLighting( false )
+                render.OverrideAlphaWriteEnable(false, false)
+                render.ModelMaterialOverride(nil)
+                render.SetWriteDepthToDestAlpha(true)
+
+
+            cam.End()
+        render.PopRenderTarget()
+
+        if !PNG then
+            callback(false, "Failed to capture render for weapon class " .. tostring(weapon_class))
+        else
+            callback(true, PNG)
+        end
+
+    end)
+
 end
 
 
 ---@param weapon_class string
 ---@param callback fun(mat: IMaterial?)
-function icon_generation.CreateMaterialForWeapon(weapon_class, callback)
+function icon_generation.CreateMaterialForWeapon(weapon_class, callback, GenerationSettings, RefreshIcon)
     local mat_path = "icon_generation/" .. weapon_class .. ".png"
 
     -- Check file exists then load it and file length less 1 day
     if file.Exists(mat_path, "DATA") and file.Time(mat_path, "DATA") > (os.time() - 86400) then
+        if RefreshIcon then
+            RunConsoleCommand("mat_reloadtexture", "data/icon_generation/" .. weapon_class)
+        end
+
         callback( Material("data/" .. mat_path, "smooth") )
         return
     end
@@ -142,11 +189,15 @@ function icon_generation.CreateMaterialForWeapon(weapon_class, callback)
             file.Write(mat_path, data)
 
             if callback then
-                callback( Material("data/" .. mat_path, "smooth") )
+                local mat = Material("data/" .. mat_path, "smooth")
+                RunConsoleCommand("mat_reloadtexture", "data/icon_generation/" .. weapon_class)
+                callback( mat )
             end
         else
             print("Failed to generate icon for weapon class " .. tostring(weapon_class) .. ": " .. tostring(data))
             callback(nil)
         end
-    end)
+    end, GenerationSettings)
 end
+
+hook.Run("icon_generation_loaded")
